e9a9344238bfa6c14fefe0e68cceca9cb03bca91
Bodia Valeur <bodiavaleur@gmail.com>
2019-08-14 13:42:44 +0300

    Updates

---
+++ precache-manifest.7e086af86d68857f1e862a98abcadb18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f1a885dde6e47971cf8606e7ea2cc58",
+    "url": "/sender/index.html"
+  },
+  {
+    "revision": "8d072fff2a3c04366962",
+    "url": "/sender/static/css/2.b8d7bdcb.chunk.css"
+  },
+  {
+    "revision": "5c542bc0a3558b4b71c9",
+    "url": "/sender/static/css/main.93d0e5f7.chunk.css"
+  },
+  {
+    "revision": "8d072fff2a3c04366962",
+    "url": "/sender/static/js/2.555a24dc.chunk.js"
+  },
+  {
+    "revision": "5c542bc0a3558b4b71c9",
+    "url": "/sender/static/js/main.b865ad0c.chunk.js"
+  },
+  {
+    "revision": "83032d28bc51d55e3f1c",
+    "url": "/sender/static/js/runtime~main.81749b37.js"
+  }
+]);
